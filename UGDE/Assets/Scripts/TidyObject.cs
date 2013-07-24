2d8bcc1e1d22f7b755654c11e391ca4cf0a52545
Ernie Huntley <ehuntley83@yahoo.com>
2013-07-23 22:22:07 -0400

    Adding the Coconut Shy shack and target

---
+++ UGDE/Assets/Scripts/TidyObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class TidyObject : MonoBehaviour
+{
+  public float removeTime = 3.0f;
+
+  void Start()
+  {
+    Destroy(gameObject, removeTime);
+  }
+}
