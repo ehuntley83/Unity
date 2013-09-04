e27a16a42062504263722f901126e86c38770d88
Ernie Huntley <ehuntley83@yahoo.com>
2013-09-04 10:04:42 -0400

    Adding the menu scene using GUI Textures
    Not adding the CLS service files at this time due to potential privacy
    conflicts

---
+++ UGDE/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainMenuButtons : MonoBehaviour
+{
+  public string levelToLoad;
+  public Texture2D normalTexture;
+  public Texture2D rollOverTexture;
+  public AudioClip beep;
+  public bool quitButton = false;
+
+  void OnMouseEnter()
+  {
+    guiTexture.texture = rollOverTexture;
+  }
+
+  void OnMouseExit()
+  {
+    guiTexture.texture = normalTexture;
+  }
+
+  IEnumerator OnMouseUp()
+  {
+    audio.PlayOneShot(beep);
+    yield return new WaitForSeconds(0.35f);
+
+    if (quitButton)
+    {
+      Application.Quit();
+    }
+    else
+    {
+      Application.LoadLevel(levelToLoad);
+    }
+  }
+}
