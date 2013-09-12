58db74194e5edf9e53fdf89f7ae0a47e895468c5
Ernie Huntley <ehuntley83@yahoo.com>
2013-09-12 15:46:02 -0400

    Adding the win sequence

---
+++ UGDE/Assets/Scripts/Fader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fader : MonoBehaviour
+{
+  public GUITexture loadGUI;
+
+  // Use this for initialization
+  void Start ()
+  {
+    Rect currentRes = new Rect(-Screen.width * 0.5f,
+      -Screen.height * 0.5f,
+      Screen.width,
+      Screen.height);
+
+    guiTexture.pixelInset = currentRes;
+  }
+
+  void LoadAnimation()
+  {
+    Instantiate(loadGUI);
+  }
+}
---
+++ UGDE/Assets/Scripts/WinGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinGame : MonoBehaviour
+{
+  public GameObject winSequence;
+  public GUITexture fader;
+  public AudioClip winClip;
+
+  IEnumerator GameOver()
+  {
+    AudioSource.PlayClipAtPoint(winClip, transform.position);
+    Instantiate(winSequence);
+    yield return new WaitForSeconds(6.0f);
+    Instantiate(fader);
+  }
+}
