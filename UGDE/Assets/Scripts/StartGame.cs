5628c85995ea60361268ab5f8ea7af23d4229e1e
Ernie Huntley <ehuntley83@yahoo.com>
2013-09-18 14:28:04 -0400

    Adding a fade-in animation & renaming fade prefab

---
+++ UGDE/Assets/Scripts/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour
+{
+  public GUITexture fader;
+
+  // Use this for initialization
+  void Start ()
+  {
+    Rect currentRes = new Rect(-Screen.width * 0.5f,
+      -Screen.height * 0.5f,
+      Screen.width,
+      Screen.height);
+
+    fader.pixelInset = currentRes;
+
+    var fadeObj = Instantiate(fader);
+    fadeObj.name = "fadeIn";
+
+    Destroy(GameObject.Find("fadeIn"), 2.0f);
+    Destroy(gameObject, 2.0f);
+  }
+}
